2f1bc9deb79641ac4febfc39bb04f0f16249be56
ivy <ivyone.evergreen@gmail.com>
2019-04-29 15:13:19 +0500

    compile assets

---
+++ js/pause.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var textInfoSetup = function textInfoSetup(_) {
+  var textInfoSelector = document.querySelector('.text-info');
+  var textInfo = localStorage.getItem('textInfo');
+
+  if (textInfo !== null) {
+    textInfoSelector.textContent = textInfo;
+  }
+
+  textInfoSelector.addEventListener("input", function () {
+    localStorage.setItem('textInfo', textInfoSelector.textContent);
+  }, false);
+};
+
+var embedVideo = function embedVideo(_) {
+  var startAt = Math.floor(Math.random() * Math.floor(30 * 60));
+  var url = "https://www.youtube.com/embed/wOMwO5T3yT4?version=3&autoplay=1&playlist=wOMwO5T3yT4&loop=1&start=" + startAt;
+  document.querySelector('iframe.youtube').setAttribute('src', url);
+};
+
+window.onload = function () {
+  textInfoSetup();
+  embedVideo();
+};
